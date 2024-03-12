dead2f543d6fadbbc77e2c195d35fa7937972865
alas yehia <alasyehia5@gmail.com>
2024-03-13 01:23:06 +0200

    add new feature

--- src/main/java/com/backend/EEA/business/dao/repositories/masterdat/CompanyRepository.java
+++ src/main/java/com/backend/EEA/business/dao/repositories/masterdat/CompanyRepository.java
@@ -4,7 +4,11 @@ import com.backend.EEA.business.dao.common.GenericJPARepository;
 import com.backend.EEA.model.entity.masterdata.Company;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CompanyRepository extends GenericJPARepository<Company> {
+
+    List<Company> findByActivityId(Long id);
 
 }
